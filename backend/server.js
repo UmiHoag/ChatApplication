a722578f4259bd9c44f35e65bd130c0e97b0e7c2
UmiHoag <nmhoang.gvo@gmail.com>
2023-12-01 19:11:38 +0700

    server revert

--- backend/server.js
+++ backend/server.js
@@ -44,11 +44,11 @@ const __dirname1 = path.resolve();
 if (process.env.NODE_ENV === "set") {
   app.use(express.static(path.join(__dirname1, "/frontend/build")));
 
-  app.get("/", (req, res) =>
+  app.get("*", (req, res) =>
     res.sendFile(path.resolve(__dirname1, "frontend", "build", "index.html"))
   );
 } else {
-  app.get("/", (req, res) => {
+  app.get("*", (req, res) => {
     res.send("API is running..");
   });
 }
